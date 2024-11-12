8f50922800198d5499ce2fe0306738fdcc88b4db
Yokeshwharan S <yokeshwharan.s@outlook.com>
2024-11-12 11:50:29 +0530

    changes in app

--- src/main/java/com/example/Demo/App.java
+++ src/main/java/com/example/Demo/App.java
@@ -1,10 +1,8 @@
 package com.example.Demo;
 
-/**
- * Hello world!
- */
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("NEW CHANGES");
     }
 }
